28a83c20b921a10d861216eb517a98aef084f788
meteora277 <justin.s.diaz@gmail.com>
2022-01-04 10:36:18 -0500

    create query for total submissions per cohort

---
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -0,0 +1,12 @@
+SELECT
+  students.name AS student,
+  AVG(assignment_submissions.duration) AS average_assignment_duration
+FROM
+  assignment_submissions
+  JOIN students ON student_id = students.id
+WHERE
+  students.end_date IS NULL
+GROUP BY
+  students.name
+ORDER BY
+  AVG(assignment_submissions.duration) DESC;
