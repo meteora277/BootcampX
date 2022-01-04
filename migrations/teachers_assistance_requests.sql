8efcaa15b274b9f5d56004b2d91b8468ce0ecb91
meteora277 <justin.s.diaz@gmail.com>
2022-01-04 17:30:33 -0500

    fix spelling errors in table names

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -10,10 +10,10 @@ CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  teacher INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
   started_at TIMESTAMP,
-  completes_at TIMESTAMP,
+  completed_at TIMESTAMP,
   student_feedback TEXT,
   teacher_feedback TEXT
 
