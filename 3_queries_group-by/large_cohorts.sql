20ece1ce559083201c12b773000abaea4d7ee844
meteora277 <justin.s.diaz@gmail.com>
2022-01-04 09:17:27 -0500

    create large cohort query

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,1 @@
+SELECT cohorts.name, count(students.*) AS student_count FROM cohorts JOIN students ON cohorts.id = students.cohort_id GROUP BY cohorts.id HAVING count(*) >= 18 ORDER BY student_count;
